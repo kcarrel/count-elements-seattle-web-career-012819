277bceebdfb08bce4b59dfdde00714eebe31e4bc
Kathleen Carrel <katiecarrel@gmail.com>
2019-02-04 17:42:28 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.each do |k|
+    if new_array.include?(k) == FALSE
+      new_array << #{k} => 
+    else
+      k[:count] += 1
+    end
+  end 
+  new_array
 end
  
